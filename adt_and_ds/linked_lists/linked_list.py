bc1108e922a990c6029fc1d6a09474df753a4b31
Clifford <clifford.onyonka@8teq.co.ke>
2021-09-02 13:11:12 +0300

    Refactor: abstract a linked list adt from the different linked list implementations

---
+++ adt_and_ds/linked_lists/linked_list.py
@@ -0,0 +1,290 @@
+import copy
+from abc import ABC, abstractmethod
+from typing import Union
+
+
+class Empty(Exception):
+    pass
+
+
+class _BaseNode(ABC):
+    def __init__(self, previous_node, next_node):
+        self.previous_node = previous_node
+        self.next_node = next_node
+
+
+class LinkedList(ABC):
+    """A linked list is a collection of nodes, with each node containing a reference to the node preceding it, or the
+    node succeeding it, or both. Data structures that implement this abstract data type include:
+
+    1. singly linked list
+    2. circularly singly linked list
+    3. doubly linked list
+    4. circularly doubly linked list
+
+    For each of these data structures, the implementation details may defer between one implementation and another. One
+    implementation may use sentinel nodes to denote the head and tail nodes, whereas another may use normal nodes with
+    references to items within the list for the head and tail. The implementation details may also change depending
+    on the context that these data structures will be used in, so as to best optimise them to the tasks within that
+    specific context.
+
+    The implementation of the various linked list data structures defined within this project is meant to provide a
+    uniform behaviour for all the data structures, such that one data structure may be replaced with another seamlessly.
+    """
+
+    class _Node(_BaseNode):
+        def __init__(
+                self, data, previous_node: Union[_BaseNode, None] = None, next_node: Union[_BaseNode, None] = None
+        ):
+            super().__init__(previous_node, next_node)
+            self.data = data
+
+    class _SentinelNode(_BaseNode):
+        def __init__(self, previous_node: Union[_BaseNode, None] = None, next_node: Union[_BaseNode, None] = None):
+            super().__init__(previous_node, next_node)
+
+    def __init__(self):
+        self._head = LinkedList._SentinelNode()
+        self._tail = LinkedList._SentinelNode()
+        self._length = 0
+        self.__current_node: Union[LinkedList._Node, None] = None
+
+    def __len__(self):
+        """ Returns total number of items in list. Time complexity: O(1).
+
+        :return: total number of items in list
+        """
+        return self._length
+
+    def __repr__(self):
+        """ Returns a string representation of the list. Time complexity: O(n).
+
+        :return: string representation of the list
+        """
+        if self._length == 0:
+            return "[]"
+
+        current_node = self._head.next_node
+        s = f"["
+
+        while not isinstance(current_node, LinkedList._SentinelNode):
+            s += f"{current_node.data}, "
+            current_node = current_node.next_node
+
+        return f"{s[:-2]}]"
+
+    def __iter__(self):
+        """ Returns a linked list iterable. Time complexity: O(1).
+
+        :return: linked list iterable
+        """
+        return self
+
+    def __next__(self):
+        """ Returns next item of linked list iterator. Time complexity: O(1).
+
+        :return: next item
+        :raises StopIteration: when the cursor denoting the current item surpasses the last item of the list
+        """
+        self.__current_node = self._head if self.__current_node is None else self.__current_node.next_node
+        next_node = self.__current_node.next_node
+
+        if isinstance(next_node, LinkedList._SentinelNode):
+            self.__current_node = None
+            raise StopIteration
+
+        return next_node.data
+
+    def __getitem__(self, idx: Union[int, slice]):
+        """ Returns item at a specific index, or items in a slice range of the list. Time complexity: O(n).
+
+        :param idx: index or slice range of items within the list
+        :return: item at a specific index, or items in a slice range
+        :raises IndexError: when the index or slice passed is out of range
+        :raises ValueError: when the step of the slice passed less than one
+        """
+        if isinstance(idx, int):
+            if idx < 0 or idx >= self._length:
+                raise IndexError("Index out of range")
+
+            current_node = self._head.next_node
+
+            for i in range(idx):
+                current_node = current_node.next_node
+
+            return current_node.data
+
+        elif isinstance(idx, slice):
+            start = 0 if idx.start is None else idx.start
+            stop = self._length if idx.stop is None else idx.stop
+            step = 1 if idx.step is None else idx.step
+
+            if step <= 0:
+                raise ValueError("Step needs to be greater than zero")
+
+            if start < 0 or start > self._length or stop < 0 or stop > self._length:
+                raise IndexError("Index out of range")
+
+            current_node = self._head.next_node
+            a_list = copy.deepcopy(self)
+            i = 0
+            skipped = 0
+
+            a_list.remove_all()
+
+            while i < stop:
+                if i == start or (i >= start and skipped == step):
+                    a_list.append(current_node.data)
+                    skipped = 0
+
+                current_node = current_node.next_node
+                i += 1
+                skipped += 1
+
+            return a_list
+
+        else:
+            raise TypeError
+
+    def __setitem__(self, idx: int, data):
+        """ Replaces item at a specific index. Time complexity: O(n).
+
+        :param idx: index of item to be replaced
+        :param data: new item to replace existing item
+        :raises IndexError: when the index passed is out of range
+        """
+        if idx < 0 or idx >= self._length:
+            raise IndexError("Index out of range")
+
+        current_node = self._head.next_node
+
+        for i in range(idx):
+            current_node = current_node.next_node
+
+        current_node.data = data
+
+    def __delitem__(self, idx: int):
+        """ Deletes item at a specific index. Time complexity: O(n).
+
+        :param idx: index of item to be deleted
+        :raises IndexError: when the index passed is out of range
+        """
+        if idx < 0 or idx >= self._length:
+            raise IndexError("Index out of range")
+
+        self._length -= 1
+        previous_node = self._head
+        current_node = previous_node.next_node
+
+        for i in range(idx):
+            previous_node = current_node
+            current_node = current_node.next_node
+
+        self._remove_between(previous_node, current_node.next_node)
+
+    @staticmethod
+    @abstractmethod
+    def _insert_between(new_node: _BaseNode, node1: _BaseNode, node2: _BaseNode):
+        """ Helper function that inserts a node between two other nodes
+
+        :param new_node: node to be inserted
+        :param node1: node at the start
+        :param node2: node at the end
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def _remove_between(node1: _BaseNode, node2: _BaseNode):
+        """ Helper function that removes a node between two other nodes
+
+        :param node1: node at the start
+        :param node2: node at the end
+        """
+        raise NotImplementedError
+
+    def insert(self, idx: int, data):
+        """ Add item at a specific index of the list. Time complexity: O(n).
+
+        :param idx: index to insert item at
+        :param data: item to insert
+        :raises IndexError: when the index passed is out of range
+        """
+        if idx < 0 or idx >= self._length:
+            raise IndexError("Index out of range")
+
+        self._length += 1
+        previous_node = self._head
+        current_node = previous_node.next_node
+
+        for i in range(idx):
+            previous_node = current_node
+            current_node = current_node.next_node
+
+        self._insert_between(LinkedList._Node(data), previous_node, current_node)
+
+    def insert_first(self, data):
+        """ Add item at the head of the list. Time complexity: O(1).
+
+        :param data: item to insert
+        """
+        self._length += 1
+        self._insert_between(LinkedList._Node(data), self._head, self._head.next_node)
+
+    @abstractmethod
+    def insert_last(self, data):
+        """ Add item at the tail of the list
+
+        :param data: item to insert
+        """
+        raise NotImplementedError
+
+    def append(self, data):
+        """ Alias of insert_last
+
+        :param data: item to insert
+        """
+        self.insert_last(data)
+
+    def remove_all(self):
+        """ Delete all items from the list. Time complexity: O(1).
+        """
+        self.__init__()
+
+    def remove_first(self):
+        """ Delete item at the head of the list. Time complexity: O(1).
+
+        :raises Empty: when the list is empty
+        """
+        if self._length == 0:
+            raise Empty("List is empty")
+
+        self._length -= 1
+        current_node = self._head.next_node
+
+        self._remove_between(self._head, current_node.next_node)
+
+    @abstractmethod
+    def remove_last(self):
+        """ Delete item at the tail of the list
+        """
+        raise NotImplementedError
+
+    def get_first(self):
+        """ Returns item at the head of the list. Time complexity: O(1).
+
+        :return: first item in list
+        :raises Empty: when the list is empty
+        """
+        if self._length == 0:
+            raise Empty("List is empty")
+
+        return self._head.next_node.data
+
+    @abstractmethod
+    def get_last(self):
+        """ Returns item at the tail of the list
+
+        :return: last item in list
+        """
+        raise NotImplementedError
--- adt_and_ds/linked_lists/singly_linked_list.py
+++ adt_and_ds/linked_lists/singly_linked_list.py
@@ -1,434 +1,219 @@
 from typing import Union
+from linked_list import LinkedList, Empty
 
 
-class Empty(Exception):
-    pass
+class SinglyLinkedList(LinkedList):
+    """ A singly linked list is a linear collection of nodes whose head and tail nodes are unconnected. Each node
+    contains a reference to the node succeeding it.
 
+    Instantiating a singly linked list
+        >>> a_list = SinglyLinkedList()
 
-class _BaseNode:
-    def __init__(self, next_node):
-        self.next_node = next_node
+    Appending an item to the list
+        >>> a_list.append(0)
 
+    The L.append(x) method is an alias of L.insert_last(x)
+        >>> a_list.insert_last(1)
 
-class _Node(_BaseNode):
-    def __init__(self, data, next_node: Union[_BaseNode, None] = None):
-        super().__init__(next_node)
-        self.data = data
+    Inserting an item at the head of the list
+        >>> a_list.insert_first(2)
 
+    Inserting an item at a specific index
+        >>> a_list.insert(1, 3)
 
-class _SentinelNode(_BaseNode):
-    def __init__(self, next_node: Union[_BaseNode, None] = None):
-        super().__init__(next_node)
+    Inserting an item at an index that's out of range raises an IndexError
+        >>> a_list.insert(100, 3)
+        Traceback (most recent call last):
+        ...
+        IndexError: Index out of range
 
+    Get first item of the list
+        >>> a_list.get_first()
+        2
 
-class SinglyLinkedList:
-    def __init__(self):
-        self.__head = _SentinelNode(_SentinelNode())
-        self.__length = 0
-        self.__current_node: Union[_Node, None] = None
+    Get first item of an empty list raises Empty
+        >>> SinglyLinkedList().get_first()
+        Traceback (most recent call last):
+        ...
+        linked_list.Empty: List is empty
 
-    def __len__(self):
-        """ Returns total number of items in list. Time complexity: O(1).
-
-        >>> l_list = SinglyLinkedList()
-        >>> len(l_list)
-        0
-        >>> l_list.append(1)
-        >>> len(l_list)
+    Get last item of the list
+        >>> a_list.get_last()
         1
 
-        :return: total number of items in list
-        """
-        return self.__length
-
-    def __repr__(self):
-        """ Returns a string representation of the list. Time complexity: O(n).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list
-        []
-        >>> l_list.append(0)
-        >>> l_list
-        [0]
-        >>> l_list.append(1)
-        >>> l_list
-        [0, 1]
-
-        :return: string representation of the list
-        """
-        if self.__length == 0:
-            return "[]"
-
-        current_node = self.__head.next_node
-        s = f"["
-
-        while not isinstance(current_node, _SentinelNode):
-            s += f"{current_node.data}, "
-            current_node = current_node.next_node
-
-        return f"{s[:-2]}]"
-
-    def __iter__(self):
-        """ Returns a singly linked list iterable. Time complexity: O(1).
-
-        >>> from collections.abc import Iterable
-        >>> l_list = SinglyLinkedList()
-        >>> isinstance(l_list, Iterable)
-        True
-        >>> l_list.append(0)
-        >>> iterable = iter(l_list)
-        >>> next(iterable)
-        0
-        >>> next(iterable)
+    Get last item of an empty list raises Empty
+        >>> SinglyLinkedList().get_last()
         Traceback (most recent call last):
         ...
-        StopIteration
+        linked_list.Empty: List is empty
 
-        :return: singly linked list iterable
-        """
-        return self
+    Get item at a specific index
+        >>> a_list[1]
+        3
 
-    def __next__(self):
-        """ Returns next item of singly linked list iterator. Time complexity: O(1).
-
-        >>> from collections.abc import Iterator
-        >>> l_list = SinglyLinkedList()
-        >>> isinstance(l_list, Iterator)
-        True
-        >>> l_list.append(0)
-        >>> next(l_list)
-        0
-        >>> next(l_list)
-        Traceback (most recent call last):
-        ...
-        StopIteration
-
-        :return: next item
-        """
-        self.__current_node = self.__head if self.__current_node is None else self.__current_node.next_node
-        next_node = self.__current_node.next_node
-
-        if isinstance(next_node, _SentinelNode):
-            self.__current_node = None
-            raise StopIteration
-
-        return next_node.data
-
-    def __getitem__(self, idx: Union[int, slice]):
-        """ Returns item at a specific index, or items in a slice range of the list. Time complexity: O(n).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.append(0)
-        >>> l_list.append(1)
-        >>> l_list.append(2)
-        >>> l_list.append(3)
-        >>> l_list.append(4)
-        >>> l_list.append(5)
-        >>> l_list[0]
-        0
-        >>> l_list[6]
+    Get item at an index that's out of range raises an IndexError
+        >>> a_list[100]
         Traceback (most recent call last):
         ...
         IndexError: Index out of range
-        >>> l_list[1:5:2]
-        [1, 3]
-        >>> l_list[-1:6]
+
+    Get items at slice range of the list
+        >>> a_list[1:3]
+        [3, 0]
+
+    Get items at a slice that's out of range raises an IndexError
+        >>> a_list[1:100]
         Traceback (most recent call last):
         ...
         IndexError: Index out of range
-        >>> l_list[1:5:0]
+
+    Get items at a slice with a slice step of less than one raises a ValueError
+        >>> a_list[1:3:0]
         Traceback (most recent call last):
         ...
         ValueError: Step needs to be greater than zero
-        >>> l_list[""]
+
+    Get an iterable object of the list
+        >>> iterable_object = iter(a_list)
+
+    Get next item of the iterable object
+        >>> next(iterable_object)
+        2
+
+    Get next item of the list iterator
+        >>> next(a_list)
+        3
+
+    Get length of the the list
+        >>> len(a_list)
+        4
+
+    Get a string representation of the list
+        >>> str(a_list)
+        '[2, 3, 0, 1]'
+        >>> a_list
+        [2, 3, 0, 1]
+
+    Delete first item of the list
+        >>> a_list.remove_first()
+
+    Delete first item of an empty list raises Empty
+        >>> SinglyLinkedList().remove_first()
         Traceback (most recent call last):
         ...
-        TypeError
+        linked_list.Empty: List is empty
 
-        :return: item at a specific index, or items in a slice range
-        """
-        if isinstance(idx, int):
-            if idx < 0 or idx >= self.__length:
-                raise IndexError("Index out of range")
+    Delete last item of the list
+        >>> a_list.remove_last()
 
-            current_node = self.__head.next_node
+    Delete last item of an empty list raises Empty
+        >>> SinglyLinkedList().remove_last()
+        Traceback (most recent call last):
+        ...
+        linked_list.Empty: List is empty
 
-            for i in range(idx):
-                current_node = current_node.next_node
+    Delete item at a specific index
+        >>> del a_list[0]
 
-            return current_node.data
-
-        elif isinstance(idx, slice):
-            start = 0 if idx.start is None else idx.start
-            stop = self.__length if idx.stop is None else idx.stop
-            step = 1 if idx.step is None else idx.step
-
-            if step <= 0:
-                raise ValueError("Step needs to be greater than zero")
-
-            if start < 0 or start > self.__length or stop < 0 or stop > self.__length:
-                raise IndexError("Index out of range")
-
-            a_list = SinglyLinkedList()
-            current_node = self.__head.next_node
-            i = 0
-            skipped = 0
-
-            while i < stop:
-                if i == start or (i >= start and skipped == step):
-                    a_list.append(current_node.data)
-                    skipped = 0
-
-                current_node = current_node.next_node
-                i += 1
-                skipped += 1
-
-            return a_list
-
-        else:
-            raise TypeError
-
-    def __setitem__(self, idx: int, data):
-        """ Replaces item at a specific index. Time complexity: O(n).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.append(0)
-        >>> l_list[0]
-        0
-        >>> l_list[0] = 1
-        >>> l_list[0]
-        1
-        >>> l_list[1] = 1
+    Delete item of at an index that's out of range raises an IndexError
+        >>> del a_list[100]
         Traceback (most recent call last):
         ...
         IndexError: Index out of range
-        """
-        if idx < 0 or idx >= self.__length:
-            raise IndexError("Index out of range")
 
-        current_node = self.__head.next_node
+    Replace item at a specific index
+        >>> a_list[0] = 100
 
-        for i in range(idx):
-            current_node = current_node.next_node
-
-        current_node.data = data
-
-    def __delitem__(self, idx: int):
-        """ Deletes item at a specific index. Time complexity: O(n).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.append(0)
-        >>> len(l_list)
-        1
-        >>> del l_list[0]
-        >>> len(l_list)
-        0
-        >>> del l_list[1]
+    Replace item of at an index that's out of range raises an IndexError
+        >>> a_list[100] = 100
         Traceback (most recent call last):
         ...
         IndexError: Index out of range
-        """
-        if idx < 0 or idx >= self.__length:
-            raise IndexError("Index out of range")
 
-        self.__length -= 1
-        previous_node = self.__head
-        current_node = previous_node.next_node
+    Delete all items from the list
+        >>> a_list.remove_all()
+        >>> a_list
+        []
+    """
 
-        for i in range(idx):
-            previous_node = current_node
-            current_node = current_node.next_node
-
-        SinglyLinkedList._remove_between(previous_node, current_node.next_node)
+    def __init__(self):
+        super().__init__()
+        self._head.next_node = self._tail
 
     @staticmethod
-    def _insert_between(new_node: _BaseNode, node1: _BaseNode, node2: _BaseNode):
-        """ Helper function that adds a node between two other nodes. Time complexity: O(1).
+    def _insert_between(
+            new_node: LinkedList._Node,
+            node1: Union[LinkedList._Node, LinkedList._SentinelNode],
+            node2: Union[LinkedList._Node, LinkedList._SentinelNode]
+    ):
+        """ Helper function that inserts a node between two other nodes. Time complexity: O(1).
 
-        >>> node_1 = _Node(0, _Node(1))
-        >>> isinstance(node_1.next_node.next_node, _Node)
-        False
-        >>> SinglyLinkedList._insert_between(_Node(2), node_1, node_1.next_node)
-        >>> isinstance(node_1.next_node.next_node, _Node)
-        True
+        :param new_node: node to be inserted
+        :param node1: node at the start
+        :param node2: node at the end
         """
         node1.next_node, new_node.next_node = new_node, node2
 
     @staticmethod
-    def _remove_between(node1: _BaseNode, node2: _BaseNode):
+    def _remove_between(
+            node1: Union[LinkedList._Node, LinkedList._SentinelNode],
+            node2: Union[LinkedList._Node, LinkedList._SentinelNode]
+    ):
         """ Helper function that removes a node between two other nodes. Time complexity: O(1).
 
-        >>> node_1 = _Node(0, _Node(1, _Node(2)))
-        >>> isinstance(node_1.next_node.next_node, _Node)
-        True
-        >>> SinglyLinkedList._remove_between(node_1, node_1.next_node.next_node)
-        >>> isinstance(node_1.next_node.next_node, _Node)
-        False
+        :param node1: node at the start
+        :param node2: node at the end
         """
         node1.next_node = node2
-
-    def insert(self, idx: int, data):
-        """ Add item at a specific index of the list. Time complexity: O(n).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.insert(0, 3)
-        Traceback (most recent call last):
-        ...
-        IndexError: Index out of range
-        >>> l_list.append(0)
-        >>> l_list.append(1)
-        >>> l_list.insert(1, 3)
-        >>> l_list
-        [0, 3, 1]
-        >>> l_list.insert(5, 3)
-        Traceback (most recent call last):
-        ...
-        IndexError: Index out of range
-        """
-        if idx < 0 or idx >= self.__length:
-            raise IndexError("Index out of range")
-
-        self.__length += 1
-        previous_node = self.__head
-        current_node = previous_node.next_node
-
-        for i in range(idx):
-            previous_node = current_node
-            current_node = current_node.next_node
-
-        SinglyLinkedList._insert_between(_Node(data), previous_node, current_node)
-
-    def insert_first(self, data):
-        """ Add item at the head of the list. Time complexity: O(1).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.insert_first(0)
-        >>> l_list.insert_first(1)
-        >>> l_list
-        [1, 0]
-        """
-        self.__length += 1
-        SinglyLinkedList._insert_between(_Node(data), self.__head, self.__head.next_node)
 
     def insert_last(self, data):
         """ Add item at the tail of the list. Time complexity: O(n).
 
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.insert_last(0)
-        >>> l_list.insert_last(1)
-        >>> l_list
-        [0, 1]
+        :param data: item to insert
         """
-        self.__length += 1
+        self._length += 1
 
-        previous_node = self.__head
+        previous_node = self._head
         current_node = previous_node.next_node
 
-        while not isinstance(current_node, _SentinelNode):
+        while not isinstance(current_node, LinkedList._SentinelNode):
             previous_node = current_node
             current_node = current_node.next_node
 
-        SinglyLinkedList._insert_between(_Node(data), previous_node, current_node)
-
-    def append(self, data):
-        """ Alias of insert_last
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.append(0)
-        >>> l_list.append(1)
-        >>> l_list
-        [0, 1]
-        """
-        self.insert_last(data)
-
-    def remove_first(self):
-        """ Delete item at the head of the list. Time complexity: O(1).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.remove_first()
-        Traceback (most recent call last):
-        ...
-        singly_linked_list.Empty: List is empty
-        >>> l_list.append(0)
-        >>> l_list.append(1)
-        >>> l_list.remove_first()
-        >>> l_list
-        [1]
-        """
-        if self.__length == 0:
-            raise Empty("List is empty")
-
-        self.__length -= 1
-        current_node = self.__head.next_node
-
-        SinglyLinkedList._remove_between(self.__head, current_node.next_node)
+        SinglyLinkedList._insert_between(LinkedList._Node(data), previous_node, current_node)
 
     def remove_last(self):
         """ Delete item at the tail of the list. Time complexity: O(n).
 
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.remove_last()
-        Traceback (most recent call last):
-        ...
-        singly_linked_list.Empty: List is empty
-        >>> l_list.append(0)
-        >>> l_list.append(1)
-        >>> l_list.remove_last()
-        >>> l_list
-        [0]
+        :raises Empty: when the list is empty
         """
-        if self.__length == 0:
+        if self._length == 0:
             raise Empty("List is empty")
 
-        self.__length -= 1
-        previous_node = self.__head
+        self._length -= 1
+        previous_node = self._head
         current_node = previous_node.next_node
 
         while True:
-            if isinstance(current_node.next_node, _SentinelNode):
+            if isinstance(current_node.next_node, LinkedList._SentinelNode):
                 break
             previous_node = current_node
             current_node = current_node.next_node
 
         SinglyLinkedList._remove_between(previous_node, current_node.next_node)
 
-    def get_first(self):
-        """ Returns item at the head of the list. Time complexity: O(1).
-
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.get_first()
-        Traceback (most recent call last):
-        ...
-        singly_linked_list.Empty: List is empty
-        >>> l_list.append(0)
-        >>> l_list.append(1)
-        >>> l_list.get_first()
-        0
-        """
-        if self.__length == 0:
-            raise Empty("List is empty")
-
-        return self.__head.next_node.data
-
     def get_last(self):
         """ Returns item at the tail of the list. Time complexity: O(n).
 
-        >>> l_list = SinglyLinkedList()
-        >>> l_list.get_last()
-        Traceback (most recent call last):
-        ...
-        singly_linked_list.Empty: List is empty
-        >>> l_list.append(0)
-        >>> l_list.append(1)
-        >>> l_list.get_last()
-        1
+        :return: last item in list
+        :raises Empty: when the list is empty
         """
-        if self.__length == 0:
+        if self._length == 0:
             raise Empty("List is empty")
 
-        current_node = self.__head.next_node
+        current_node = self._head.next_node
 
-        while not isinstance(current_node.next_node, _SentinelNode):
+        while not isinstance(current_node.next_node, LinkedList._SentinelNode):
             current_node = current_node.next_node
 
         return current_node.data

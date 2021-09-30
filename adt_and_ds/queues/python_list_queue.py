a1fc753a6d5850657475e015e1568fb59d491a04
Clifford <clifford.onyonka@8teq.co.ke>
2021-09-30 12:19:42 +0300

    Abstract a queue ADT from its implementation

---
+++ adt_and_ds/queues/python_list_queue.py
@@ -0,0 +1,53 @@
+from queue1 import Queue, Empty
+
+
+class PythonListQueue(Queue):
+    """ A queue implemented using the python list data structure.
+
+    Instantiate a queue object
+        >>> a_queue = PythonListQueue()
+
+    Add an item to the end of the queue
+        >>> a_queue.enqueue(1)
+
+    Get the first element of the queue without removing it from the queue
+        >>> a_queue.get_first()
+        1
+
+    Check if a queue is empty
+        >>> a_queue.is_empty()
+        False
+        >>> PythonListQueue().is_empty()
+        True
+
+    Get the length of the queue
+        >>> len(a_queue)
+        1
+        >>> len(PythonListQueue())
+        0
+
+    Get and remove the first element of the queue
+        >>> a_queue.dequeue()
+        1
+    """
+    def __init__(self):
+        self.__data_store = []
+
+    def __len__(self):
+        return len(self.__data_store)
+
+    def is_empty(self):
+        return len(self.__data_store) == 0
+
+    def enqueue(self, x):
+        self.__data_store.append(x)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        return self.__data_store.pop(0)
+
+    def get_first(self):
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        return self.__data_store[0]
---
+++ adt_and_ds/queues/queue1.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+
+class Empty(Exception):
+    pass
+
+
+class Queue(ABC):
+    """ A queue is a First-In-First-Out structure that supports insertion (queueing) of elements at one end and removal
+    (dequeueing) of elements from the opposite end. The order in which elements are enqueued is maintained when
+    dequeued them.
+    """
+    @abstractmethod
+    def __len__(self):
+        """ Returns the total number of elements stored in the queue
+
+        :returns: the total number of elements stored in the queue
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_empty(self):
+        """ Check if queue contains no elements
+
+        :return: True if queue is empty, else False
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def enqueue(self, x):
+        """ Insert element x to the end of the queue
+
+        :param x: element to add to the queue
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def dequeue(self):
+        """ Remove first element of the queue and return it
+
+        :return: first element of queue
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_first(self):
+        """ Return first element of the queue without removing it
+
+        :return: first element of queue
+        """
+        raise NotImplementedError
